9382035e3553809033adf276863497d6bc6154d9
veriito-dus <veronica_dussanpa@fet.edu.co>
2021-09-22 17:54:16 -0500

    view login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,41 @@
+<html>
+
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <title>Proyecto School</title>
+  <link rel="stylesheet" href="css/style.css" />
+  <script src="https://kit.fontawesome.com/6f70aed298.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <div class="container">
+    <section id="content">
+      <!-- <div id="login-box"> -->
+      <div class="left">
+        <form action="">
+          <h1>Inicia sesión</h1>
+          <i class="fas fa-user"> Usuario</i>
+          <div>
+            <input type="text" placeholder="Ingresar tu Usuario" required="" id="Cuenta" />
+          </div>
+          <i class="fas fa-unlock-alt"> Contraseña</i>
+          <div>
+            <input type="password" placeholder="Ingresa tu Contraseña" required="" id="Contraseña" />
+          </div>
+          <div>
+            <input type="submit" value="Log in" />
+          </div>
+          <!-- <div>
+            <a href="#">Lost your password?</a>
+            <a href="#">Register</a>
+            </div> -->
+        </form><!-- form -->
+      </div>
+      <div class="right">
+      </div>
+      <!-- </div> -->
+    </section><!-- content -->
+  </div><!-- container -->
+</body>
+
+</html>
